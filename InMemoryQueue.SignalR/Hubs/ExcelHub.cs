06e8d0f489a2e732c3784ad44a542cc373809510
Barış Akdaş <barisakdas00@gmail.com>
2024-04-12 16:41:24 +0300

    Signalr layer created.

---
+++ InMemoryQueue.SignalR/Hubs/ExcelHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace InMemoryQueue.SignalR.Hubs;
+
+/// <summary>Dosya oluşturma işlemi bittikten sonra Server tarafı Client üzerine mesaj gönderecek.
+/// Client tarafının tetiklemesini gerektirecek bir durum yok. Bu yüzden Hub içerisinde tetiklenecek bir metot yazmıyoruz.</summary>
+public class ExcelHub : Hub<IExcelHub>
+{
+}
---
+++ InMemoryQueue.SignalR/Hubs/IExcelHub.cs
@@ -0,0 +1,7 @@
+﻿namespace InMemoryQueue.SignalR.Hubs;
+
+/// <summary>Dosya oluşturma işlemi bittikten sonra Server tarafı Client üzerine mesaj gönderecek.
+/// Client tarafının tetiklemesini gerektirecek bir durum yok. Bu yüzden Hub içerisinde tetiklenecek bir metot yazmıyoruz.</summary>
+public interface IExcelHub
+{
+}
